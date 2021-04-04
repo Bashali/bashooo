91cc1d62cf0c7962dc9963fde0aec9b96ca4d3d1
Bashali <81916779+Bashali@users.noreply.github.com>
2021-04-04 21:16:52 +0100

    Add files via upload

---
+++ bashie.py
@@ -0,0 +1,33 @@
+# Number 3
+a=3
+b=5
+add=a+b
+print(sum)
+print("a", "b", "add")
+# Number 1
+name = 'Bashir B Aliyu $ ' 
+department = 'computer_science '
+print(name +  department)
+# Number 2
+scare= "boo!"
+print( scare)
+# Number 4
+e= 12
+f=0.5
+divide= e / f
+print (divide)
+#Number 5
+num1=12
+num2=8
+sum=num1 + num2
+print (sum)
+# Number 6
+x=8
+y=3
+remender= x % y
+print(remender)
+# Number 7
+First= 2 * 4
+Second= (4 + 2)
+result= (First * Second)
+print(result)
